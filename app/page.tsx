86ab738820716b0bdb8b6c4f6aa24bd5ea3dbc98
lichaojie <hfcypm@126.com>
2026-01-02 17:21:55 +0800

    项目初始化

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+      合约环境页面测试项目中....
+    </div>
+  );
+}
