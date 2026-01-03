6493dd956b64997e604b4691db2bb369427ae744
lichaojie <hfcypm@126.com>
2026-01-03 13:18:26 +0800

    使用wagmi RainbowKit 初始化Dapp项目并添加钱包连接按钮

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,9 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { Providers } from "./providers";
 
+//项目生成默认字体配置
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -27,7 +29,9 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <Providers>
+          {children}
+        </Providers>
       </body>
     </html>
   );
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,9 @@
-import Image from "next/image";
+import { ConnectButton } from "@rainbow-me/rainbowkit";
 
 export default function Home() {
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      合约环境页面测试项目中....
+      <ConnectButton />
     </div>
   );
 }
