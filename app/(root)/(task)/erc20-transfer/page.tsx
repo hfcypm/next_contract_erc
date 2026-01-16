53a3130a09f4c6bb4ac0023d16bda0db2470baff
lichaojie <hfcypm@126.com>
2026-01-16 13:20:40 +0800

    viem 合约转账

--- app/(root)/(task)/erc20-transfer/page.tsx
+++ app/(root)/(task)/erc20-transfer/page.tsx
@@ -7,6 +7,8 @@ import ERC20_ABI from '../const/ercABI';
 import { useAccountEffect, useWaitForTransactionReceipt, useWriteContract } from 'wagmi';
 import { sepolia } from 'wagmi/chains';
 import { parseEther } from 'ethers/utils';
+import { Address, createPublicClient, createWalletClient, http, parseUnits, WalletClient } from 'viem';
+import { privateKeyToAccount } from 'viem/accounts';
 
 
 export default function Erc20TransferPage() {
@@ -113,7 +115,7 @@ export default function Erc20TransferPage() {
   }
 
   /////////
-  //转账功能(使用wagmi方式)
+  //-----------------------转账功能(使用wagmi方式)----------------------------------------
   // 连接钱包状态
   const [isConnected, setConnected] = useState<boolean>(false);
   useAccountEffect({
@@ -170,6 +172,107 @@ export default function Erc20TransferPage() {
     }
   }
 
+  //-----------------------转账功能(使用viem方式)----------------------------------------
+  const [userAddress, setUserAddress] = useState<Address>();
+  const [isViemConnected, setViemConnected] = useState<boolean>(false);
+  const viemTransfer = async () => {
+    try {
+      if (!isViemConnected) {
+        alert('请先连接钱包');
+        return;
+      }
+      if (!userAddress) {
+        alert('地址为空 重新断开并连接钱包');
+        return;
+      }
+      if (!targetAddress) {
+        alert('请输入目标地址');
+        return;
+      }
+      if (!transferAmount) {
+        alert('请输入转账数量');
+        return;
+      }
+      setLoading(true);
+      //----------viem 开始转账------------------
+      // 注意：这里需要使用私钥创建账户，而不是直接使用连接的钱包地址
+      const privateKey = process.env.NEXT_PUBLIC_PRIVATE_KEY;
+      if (!privateKey) {
+        alert('私钥为空 请检查环境变量 NEXT_PUBLIC_PRIVATE_KEY');
+        return;
+      }
+      const account = privateKeyToAccount(`0x${privateKey.replace('0x', '')}`);
+      //创建钱包及添加(当前连接钱包的私钥)
+      const walletClient: WalletClient = createWalletClient({
+        chain: sepolia,
+        transport: http(process.env.NEXT_PUBLIC_SEPOLIA_RPC_URL),
+      });
+
+      //组装一下交易转账的对象(转账必段的参数)
+      // 关键：将用户输入的金额转换为代币最小单位（如USDC 0.1 → 100000）
+      const publicClient = createPublicClient({
+        chain: sepolia,
+        transport: http()
+      })
+      const amountInWei = parseEther(transferAmount);
+      console.log(` 开始转账>>>${transferAmount}个代币到${targetAddress}...`);
+
+      console.log('转账的金额（单位：wei）:', amountInWei);
+
+      const { request } = await publicClient.simulateContract({
+        account,
+        //合约地址
+        address: process.env.NEXT_PUBLIC_ERC20_CONTRACT_ADDRESS as Address,
+        abi: ERC20_ABI,
+        functionName: 'transfer',
+        //转账的钱包地址及数量
+        args: [targetAddress as Address, amountInWei],
+      });
+      // 发送合约交易（MetaMask弹出签名框）
+      console.log('viem sepolia 交易转账中....  请稍等..........');
+      const hash = await walletClient.writeContract(request);
+      // 等待交易确认 
+      await publicClient.waitForTransactionReceipt({ hash });
+
+      console.log('viem sepolia转账成功:', hash);
+      setLoading(false);
+      alert('转账成功');
+    } catch (e) {
+      console.log('viem sepolia转账失败:', e);
+      setLoading(false);
+      alert('转账异常了..... 请检查操作..........');
+    }
+  }
+
+  //----------------使用viem 手动去连接一下钱包--------------------------
+  const connectWallet = async () => {
+    if (!window.ethereum) {
+      alert('请先安装MetaMask钱包');
+      return;
+    }
+    //获取权限
+    const accounts = await window.ethereum.request({
+      method: 'eth_requestAccounts',
+    }) as string[];
+    if (!accounts.length) {
+      alert('未获取到钱包账户')
+      return;
+    }
+    //获取当前钱包地址
+    const userAddress = accounts[0] as Address;
+    setUserAddress(userAddress);
+    console.log('当前钱包地址：', userAddress);
+    //获取当前链信息
+    const chainIdHex = await window.ethereum.request({ method: 'eth_chainId' });
+    const chainId = parseInt(chainIdHex, 16);
+    if (chainId !== sepolia.id) {
+      alert('请先切换到Sepolia测试网络');
+      return;
+    }
+    console.log('钱包连接成功 当前链信息：', chainId);
+    setViemConnected(true);
+  }
+
   return (
     <div className="p-4">
       <h1 className="text-2xl font-bold mb-2">实现ERC20token的转账功能</h1>
@@ -186,9 +289,11 @@ export default function Erc20TransferPage() {
         <div className="text-red-500 font-bold">转账数量:</div>
         <input onChange={dealAmountChange} type="text" className="border-2 border-gray-300 p-2 rounded-md ml-3" />
       </div>
+      <button onClick={connectWallet} className="bg-red-500 text-white pl-6 pr-6 py-2 rounded-md  mt-2 ">连接钱包</button>
       <div className='flex flex-row items-center mt-4'>
         <button onClick={transfer} className="bg-red-500 text-white pl-6 pr-6 py-2 rounded-md ">转账(ethers)</button>
         <button onClick={wagmiTransfer} className="bg-red-500 text-white pl-6 pr-6 py-2 rounded-md ml-3">转账(wagmi)</button>
+        <button onClick={viemTransfer} className="bg-red-500 text-white pl-6 pr-6 py-2 rounded-md ml-3">转账(viem)</button>
       </div>
       <div className="ml-3 font-bold text-red-500 mt-2">
         {loading && '正在努力转账中...'}
